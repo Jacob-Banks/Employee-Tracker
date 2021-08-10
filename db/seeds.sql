9545fc33e01e6c01a979c29023b7523834a03d77
Jacob Banks <kobybanks@gmail.com>
2021-08-10 16:31:02 -0400

    filled in  tables and base seed sql files

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department
+  (name)
+VALUES
+('Sales'),
+('Engingeering'),
+('finance'),
+('legal');
+
+INSERT INTO roles
+  (title,salary,department_id)
+VALUES
+('Sales Lead',10000.0,1),
+('Salesperson',80000.0,1),
+('Lead Engineer',150000.0,2),
+('Engineer',100000.0,2),
+('Leagal Lead',123123.0,4),
+('Lawyer',111123.0,4),
+('Accountant',75000.0,3);
+
+INSERT INTO employee
+  (first_name,last_name,role_id,manager_id)
+VALUES
+('ee','allen',3,NULL),
+('dd','smith',1,1),
+('aa','allen',1,NULL),
+('bb','allen',4,5),
+('cc','allen',4,NULL);
