9aa77ef6a468387a6410fb1546ee71150bf42bf5
Jacob Banks <kobybanks@gmail.com>
2021-08-11 01:29:23 -0400

    finished all non bonus menu functions

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ const menu = () => {
         return viewAllDepartments();
       } else if (menu === "Add Employee") {
         return addEmployee();
+      } else if (menu === "Add Role") {
+        return addRole();
+      } else if (menu === "Add Department") {
+        return addDepartment();
+      } else if (menu === "Update Employee Role") {
+        return updateEmployee();
       }
     });
 };
@@ -202,4 +208,128 @@ const addEmployee = () => {
           });
       });
     });
+};
+const addRole = () => {
+  db.query(
+    `SELECT department.id, department.name FROM department`,
+    (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      const roleChoices = res.map(({ id, name }) => ({
+        name: name,
+        value: id,
+      }));
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "department",
+            message: "Select a Department ",
+            choices: roleChoices,
+          },
+          { type: "input", name: "role", message: "Enter new role" },
+          { type: "number", name: "salary", message: "Enter salary" },
+        ])
+        .then((answer) => {
+          const tempHolder = [answer.department, answer.role, answer.salary];
+          db.query(
+            `INSERT INTO role (department_id, title, salary) 
+          Values (?, ?, ?)`,
+            tempHolder,
+            (err, res) => {
+              if (err) {
+                console.log(err);
+              }
+
+              console.log("Role successfully added!");
+
+              menu();
+            }
+          );
+        });
+    }
+  );
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      { type: "input", name: "department", message: "Enter new Department" },
+    ])
+    .then((answer) => {
+      const tempHolder = [answer.department];
+      db.query(
+        `INSERT INTO department (name) 
+          Values (?)`,
+        tempHolder,
+        (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+
+          console.log("Department successfully added!");
+
+          menu();
+        }
+      );
+    });
+};
+const updateEmployee = () => {
+  db.query(`SELECT * FROM employee`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const employeeChoices = res.map(({ id, first_name, last_name }) => ({
+      name: first_name + " " + last_name,
+      value: id,
+    }));
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employee",
+          message: "select which employee to update",
+          choices: employeeChoices,
+        },
+      ])
+      .then((answer) => {
+        const tempHolder = [answer.employee];
+        db.query(`SELECT * FROM role`, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          const roleChoices = res.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+
+          inquirer
+            .prompt({
+              type: "list",
+              name: "role",
+              message: "Select new role",
+              choices: roleChoices,
+            })
+            .then((answer) => {
+              tempHolder.unshift(answer.role);
+              db.query(
+                `UPDATE employee SET role_id = ? WHERE id = ?`,
+                tempHolder,
+                (err, res) => {
+                  if (err) {
+                    console.log(err);
+                  }
+
+                  console.log("Employee role successfully updated!");
+                  console.log(tempHolder);
+
+                  menu();
+                }
+              );
+            });
+        });
+      });
+  });
 };
